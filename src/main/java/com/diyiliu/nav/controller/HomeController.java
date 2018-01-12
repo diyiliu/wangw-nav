4ae4552841c0c8778c67cb7342244035e2df17fb
DIYILIU <572772828@qq.com>
2018-01-12 11:09:53 +0800

    优化界面，和网址图片

--- src/main/java/com/diyiliu/nav/controller/HomeController.java
+++ src/main/java/com/diyiliu/nav/controller/HomeController.java
@@ -4,25 +4,25 @@ import com.diyiliu.common.util.JacksonUtil;
 import com.diyiliu.nav.dao.NavDao;
 import com.diyiliu.nav.model.SiteType;
 import com.diyiliu.nav.model.Website;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.*;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -93,14 +93,6 @@ public class HomeController {
             BASE64Decoder decoder = new BASE64Decoder();
             byte[] bytes = decoder.decodeBuffer(iconStr);
 
-           /*
-           for (int i = 0; i < bytes.length; i++) {
-                if (bytes[i] < 0) {
-                    bytes[i] += 256;
-                }
-            }
-            */
-
             OutputStream out = response.getOutputStream();
             out.write(bytes);
             out.flush();
@@ -110,84 +102,74 @@ public class HomeController {
 
 
     public String base64ICO(String location) {
-        String method = "GET";
         String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36";
+        String scheme = "http";
 
-        HttpURLConnection connection = null;
-        InputStreamReader streamReader = null;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.USER_AGENT, userAgent);
+
         try {
-            URL url = new URL("http://" +location);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod(method);
-            connection.addRequestProperty("User-Agent", userAgent);
+            ResponseEntity<byte[]> responseEntity = restTemplate.exchange(
+                    scheme + "://" + location,
+                    HttpMethod.GET,
+                    new HttpEntity<byte[]>(headers),
+                    byte[].class);
 
-            int state = connection.getResponseCode();
+            int statusCode = responseEntity.getStatusCode().value();
+            if (301 == statusCode || 302 == statusCode) {
+                URI uri = responseEntity.getHeaders().getLocation();
+                scheme = uri.getScheme();
+                location = uri.getHost();
 
-            String dirLocation = location;
-            if (state == 302 || state == 301) {
-                dirLocation = connection.getHeaderField("Location");
-                url = new URL(dirLocation);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod(method);
-                connection.addRequestProperty("User-Agent", userAgent);
-                state = connection.getResponseCode();
+                responseEntity = restTemplate.exchange(
+                        uri,
+                        HttpMethod.GET,
+                        new HttpEntity<byte[]>(headers),
+                        byte[].class);
+                statusCode = responseEntity.getStatusCode().value();
             }
 
-            if (state == 200) {
-                streamReader = new InputStreamReader(connection.getInputStream(), "utf-8");
-                BufferedReader br = new BufferedReader(streamReader);
-
-                String icoPath = null;
+            String icoPath = null;
+            if (200 == statusCode) {
+                InputStream inputStream = new ByteArrayInputStream(responseEntity.getBody());
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
                 String line;
                 while ((line = br.readLine()) != null) {
                     if (line.contains(".ico")) {
                         // 取出有用的范围
-                        Pattern p = Pattern.compile("<link[^>]*href=\"(?<href>[^\"]*)\"[^>]*>");
-                        Matcher m = p.matcher(line);
+                        Pattern p = Pattern.compile(".*<link[^>]*href=\"(?<href>[^\"]*)\"[^>]*>");
+                        Matcher m = p.matcher(line.trim());
                         if (m.matches()) {
                             String path = m.group(1);
-                            if (path.contains("//")) {
-                                icoPath = path;
-                            } else {
-                                icoPath = dirLocation + path;
+                            if (!path.contains("//")) {
+                                path = scheme + "://" + location + path;
                             }
+                            icoPath = path;
                             break;
                         }
                     }
                 }
+            }
 
-                // 未匹配成功，用网络ICO工具获取
-                if (icoPath == null) {
-                    String iconTool = "http://statics.dnspod.cn/proxy_favicon/_/favicon?domain=";
-                    url = new URL(iconTool + location);
-                }else {
-                    url = new URL(icoPath);
-                }
+            // 未匹配成功，用网络ICO工具获取
+            if (icoPath == null) {
+                icoPath = "http://statics.dnspod.cn/proxy_favicon/_/favicon?domain=" + location;
+            }
 
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod(method);
-                connection.addRequestProperty("User-Agent", userAgent);
-                state = connection.getResponseCode();
-                if (state == 200) {
-                    byte[] bytes = FileCopyUtils.copyToByteArray(url.openStream());
-
-                    BASE64Encoder encoder = new BASE64Encoder();
-                    return encoder.encode(bytes);
-                }
+            responseEntity = restTemplate.exchange(
+                    icoPath,
+                    HttpMethod.GET,
+                    new HttpEntity<byte[]>(headers),
+                    byte[].class);
+            statusCode = responseEntity.getStatusCode().value();
+            if (statusCode == 200) {
+                byte[] bytes = responseEntity.getBody();
+                BASE64Encoder encoder = new BASE64Encoder();
+                return encoder.encode(bytes);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (streamReader != null) {
-                    streamReader.close();
-                }
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
         return null;
--- src/test/java/TestHttp.java
+++ src/test/java/TestHttp.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,7 +22,7 @@ public class TestHttp {
 
     @Test
     public void test() throws Exception {
-        String location = "http://www.oschina.net";
+        String location = "http://hao.uisdc.com";
         URL url = new URL(location);
         try {
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
@@ -27,7 +31,7 @@ public class TestHttp {
 
             int respCode = httpURLConnection.getResponseCode();
 
-            if (respCode != 200){
+            if (respCode != 200) {
                 location = httpURLConnection.getHeaderField("Location");
                 httpURLConnection = (HttpURLConnection) new URL(location).openConnection();
                 httpURLConnection.setRequestMethod("GET");
@@ -37,17 +41,17 @@ public class TestHttp {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"));
             String line;
-            while ((line = br.readLine())!= null ){
-                System.out.println(line);
-                if (line.contains(".ico")){
+            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+                if (line.contains(".ico")) {
                     // 取出有用的范围
-                    Pattern p = Pattern.compile("(.*)(href=\")(.*?)(.ico\")(.*)");
+                    Pattern p = Pattern.compile("<link[^>]*href=\"(?<href>[^\"]*)\"[^>]*>");
                     Matcher m = p.matcher(line);
-                    if (m.matches()){
-                        String path = m.group(3);
-                        if (path.contains("//")){
+                    if (m.matches()) {
+                        String path = m.group(1);
+                        if (path.contains("//")) {
                             path += ".ico";
-                        }else {
+                        } else {
                             path = location + path + ".ico";
                         }
 
@@ -63,10 +67,81 @@ public class TestHttp {
     }
 
     @Test
-    public void test2(){
+    public void test2() {
 
         String str = "https://";
 
         System.out.println(str.substring(0, str.indexOf(":") + 1));
+    }
+
+
+    @Test
+    public void test3() throws Exception {
+        String url = "www.ui.cn";
+        url = "www.baidu.com";
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36";
+        String scheme = "http";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.USER_AGENT, userAgent);
+
+        ResponseEntity<byte[]> responseEntity = restTemplate.exchange(
+                scheme + "://" + url,
+                HttpMethod.GET,
+                new HttpEntity<byte[]>(headers),
+                byte[].class);
+
+
+        int statusCode = responseEntity.getStatusCode().value();
+        if (301 == statusCode || 302 == statusCode) {
+            URI uri = responseEntity.getHeaders().getLocation();
+            scheme = uri.getScheme();
+            url = uri.getHost();
+            responseEntity = restTemplate.exchange(
+                    uri,
+                    HttpMethod.GET,
+                    new HttpEntity<byte[]>(headers),
+                    byte[].class);
+            statusCode = responseEntity.getStatusCode().value();
+        }
+
+        if (200 != statusCode) {
+            System.out.println(statusCode);
+            return;
+        }
+
+        InputStream inputStream = new ByteArrayInputStream(responseEntity.getBody());
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.contains(".ico")) {
+                // 取出有用的范围
+                Pattern p = Pattern.compile(".*<link[^>]*href=\"(?<href>[^\"]*)\"[^>]*>");
+                Matcher m = p.matcher(line.trim());
+                if (m.matches()) {
+                    String path = m.group(1);
+                    if (!path.contains("//")) {
+                        path = scheme + "://" + url + path;
+                    }
+
+                    System.out.println(path);
+                    System.out.println("ok!");
+                    break;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test4() {
+        String str = "<link rel=\"shortcut icon\" href=\"/favicon.ico\" type=\"image/x-icon\" />";
+        Pattern p = Pattern.compile(".*<link[^>]*href=\"(?<href>[^\"]*)\"[^>]*>");
+        Matcher m = p.matcher(str);
+        if (m.matches()) {
+            String path = m.group(1);
+
+            System.out.println(path);
+            System.out.println("ok!");
+        }
     }
 }
